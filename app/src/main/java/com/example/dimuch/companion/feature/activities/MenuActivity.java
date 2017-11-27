308fe00e75af33f6012f83b49ba8b26b7389f28d
Dimuch_ <antibog8@gmail.com>
2017-11-27 15:12:41 +0200

    add onClick bMap (create MapActivity)

--- app/src/main/java/com/example/dimuch/companion/feature/activities/MenuActivity.java
+++ app/src/main/java/com/example/dimuch/companion/feature/activities/MenuActivity.java
@@ -1,18 +1,18 @@
 package com.example.dimuch.companion.feature.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import com.arellomobile.mvp.MvpAppCompatActivity;
 import com.example.dimuch.companion.R;
 import com.example.dimuch.companion.feature.views.IMenuActivityView;
-import timber.log.Timber;
 
-public class MenuActivity extends AppCompatActivity implements IMenuActivityView {
+public class MenuActivity extends MvpAppCompatActivity implements IMenuActivityView {
 
   @BindView(R.id.bMap) Button bMap;
   @BindView(R.id.bStoreList) Button bStoreList;
@@ -24,20 +24,19 @@ public class MenuActivity extends AppCompatActivity implements IMenuActivityView
     ButterKnife.bind(this);
   }
 
-  @OnClick(R.id.bMap)
-  public void bMapClick(View view) {
+  @OnClick(R.id.bMap) public void bMapClick(View view) {
     showToast("bMap");
-    // TODO opens activity_map ...
+
+    Intent intent = new Intent(this, MapActivity.class);
+    startActivity(intent);
   }
 
-  @OnClick(R.id.bStoreList)
-  public void bStoreListClick(View view) {
+  @OnClick(R.id.bStoreList) public void bStoreListClick(View view) {
     showToast("bStoreList");
     // TODO opens activity_store_list ...
   }
 
-  @OnClick(R.id.bForFuture)
-  public void bForFutureClick(View view) {
+  @OnClick(R.id.bForFuture) public void bForFutureClick(View view) {
     showToast("bForFuture");
     // TODO opens activity_for_future ...
   }
