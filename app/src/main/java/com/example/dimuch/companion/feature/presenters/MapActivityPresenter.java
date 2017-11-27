0e254f8e2893673d6e5064da3b8ac84c94819325
Dimuch_ <antibog8@gmail.com>
2017-11-27 16:06:22 +0200

    add MapActivityPresenter

---
+++ app/src/main/java/com/example/dimuch/companion/feature/presenters/MapActivityPresenter.java
@@ -0,0 +1,11 @@
+package com.example.dimuch.companion.feature.presenters;
+
+import com.arellomobile.mvp.MvpPresenter;
+import com.example.dimuch.companion.feature.views.IMapActivityView;
+
+/**
+ * Created by Dimuch on 27.11.2017.
+ */
+
+public class MapActivityPresenter extends MvpPresenter<IMapActivityView> {
+}
