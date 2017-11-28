6c120ea58431eaa4b09d94a68cd7557b37fdbd7c
Dimuch_ <antibog8@gmail.com>
2017-11-28 11:52:14 +0200

    add RVAdapterForStoreList + create test store list

--- app/src/main/java/com/example/dimuch/companion/feature/activities/StoreListActivity.java
+++ app/src/main/java/com/example/dimuch/companion/feature/activities/StoreListActivity.java
@@ -1,11 +1,18 @@
 package com.example.dimuch.companion.feature.activities;
 
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.EditText;
 import android.widget.Toast;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import com.arellomobile.mvp.MvpAppCompatActivity;
 import com.example.dimuch.companion.R;
+import com.example.dimuch.companion.feature.adapters.RVAdapterForStoreList;
 import com.example.dimuch.companion.feature.views.IStoreListActivityView;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Dimuch on 27.11.2017.
@@ -13,12 +20,54 @@ import com.example.dimuch.companion.feature.views.IStoreListActivityView;
 
 public class StoreListActivity extends MvpAppCompatActivity implements IStoreListActivityView {
 
+  @BindView(R.id.etSearch) EditText etSearch;
+  @BindView(R.id.rvStoreList) RecyclerView rvStoreList;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_store_list);
     ButterKnife.bind(this);
     setTitle(R.string.store_list);
+
+    LinearLayoutManager layoutManagerForStores =
+        new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false);
+    rvStoreList.setLayoutManager(layoutManagerForStores);
+    RVAdapterForStoreList rvAdapterForStoreList = new RVAdapterForStoreList(testListForRV());
+    rvStoreList.setAdapter(rvAdapterForStoreList);
+  }
+
+  private List<String> testListForRV() {
+    List<String> testList = new ArrayList<>();
+    testList.add("Магазин №1");
+    testList.add("Магазин №2");
+    testList.add("Магазин №3");
+    testList.add("Магазин №4");
+    testList.add("Магазин №5");
+    testList.add("Магазин №6");
+    testList.add("Магазин №7");
+    testList.add("Магазин №8");
+    testList.add("Магазин №9");
+    testList.add("Магазин №10");
+    testList.add("Магазин №11");
+    testList.add("Магазин №12");
+    testList.add("Магазин №13");
+    testList.add("Магазин №14");
+    testList.add("Магазин №15");
+    testList.add("Магазин №16");
+    testList.add("Магазин №17");
+    testList.add("Магазин №18");
+    testList.add("Магазин №19");
+    testList.add("Магазин №20");
+    testList.add("Магазин №21");
+    testList.add("Магазин №22");
+    testList.add("Магазин №23");
+    testList.add("Магазин №24");
+    testList.add("Магазин №25");
+    testList.add("Магазин №26");
+    testList.add("Магазин №27");
+    testList.add("Магазин №28");
+    testList.add("Магазин №29");
+    return testList;
   }
 
   @Override public void showToast(String sToastMessage) {
---
+++ app/src/main/java/com/example/dimuch/companion/feature/adapters/RVAdapterForStoreList.java
@@ -0,0 +1,48 @@
+package com.example.dimuch.companion.feature.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import com.example.dimuch.companion.R;
+import java.util.List;
+
+public class RVAdapterForStoreList extends RecyclerView.Adapter<RVAdapterForStoreList.ViewHolder> {
+
+  private List<String> mStoreList;
+
+  public RVAdapterForStoreList(List<String> storeList) {
+    mStoreList = storeList;
+  }
+
+  public static class ViewHolder extends RecyclerView.ViewHolder {
+
+    @BindView(R.id.tvStore) TextView tvStore;
+
+    ViewHolder(View itemView) {
+      super(itemView);
+      ButterKnife.bind(this, itemView);
+    }
+  }
+
+  @Override public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+    View v = LayoutInflater.from(viewGroup.getContext())
+        .inflate(R.layout.item_recycle_view_store, viewGroup, false);
+    return new ViewHolder(v);
+  }
+
+  @Override public void onBindViewHolder(ViewHolder holder, int position) {
+    holder.tvStore.setText(mStoreList.get(position));
+  }
+
+  @Override public int getItemCount() {
+    return mStoreList.size();
+  }
+
+  @Override public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+    super.onAttachedToRecyclerView(recyclerView);
+  }
+}
